961dbf724792965b640c3bcec7ff9bbafd06480d
dirold2 <i@dirold2.ru>
2026-08-26 18:50:26 +0300

    v2.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { withCache } from "./plugin.js";
 export type { CacheEntry, CacheManagerOptions } from "./types/cache.js";
-export { CacheManager, MiniEmitter } from "hcacher";
+export { CacheManager } from "hcacher";
 export type { CacheEvents } from "hcacher";
